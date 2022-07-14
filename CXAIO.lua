e78a41f42e5e8b1cc51cc9fefa10f78c50163f57
Coozbie <33816052+Coozbie@users.noreply.github.com>
2022-07-14 00:30:53 -0400

    Update CXAIO.lua

--- CXAIO.lua
+++ CXAIO.lua
@@ -1,4 +1,4 @@
-local SCRIPT_NAME, VERSION, LAST_UPDATE = "CXAIO", "1.0.1", "07/07/2022"
+local SCRIPT_NAME, VERSION, LAST_UPDATE = "CXAIO", "1.0.2", "14/07/2022"
 _G.CoreEx.AutoUpdate("https://raw.githubusercontent.com/Coozbie/RBR/main/CXAIO.lua", VERSION)
 module(SCRIPT_NAME, package.seeall, log.setup)
 clean.module(SCRIPT_NAME, clean.seeall, log.setup)
@@ -7,6 +7,7 @@ local Player = _G.Player
 
 local supportedChamp = {
     Zed = true,
+    Cassiopeia = true,
 }
 
 if supportedChamp[Player.CharName] then
